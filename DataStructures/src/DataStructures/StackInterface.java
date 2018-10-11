ba306cda55896835449a253372aa603dd80f5a03
Koshika Gupta <koshikaje@gmail.com>
2018-10-11 10:13:31 +0530

    Add files via upload

---
+++ DataStructures/src/DataStructures/StackInterface.java
@@ -0,0 +1,9 @@
+package DataStructures;
+
+public interface StackInterface {
+   public void push(int d);
+   public boolean isFull();
+   public boolean isEmpty();
+   public int peek();
+   public int pop();
+}
