2d96bc29cfb29e2deb6044ed9f3c0c76074e0c28
Koshika Gupta <koshikaje@gmail.com>
2018-10-19 17:23:49 +0530

    Update graphs.java

--- DataStructures/src/DataStructures/graphs.java
+++ DataStructures/src/DataStructures/graphs.java
@@ -1,13 +1,14 @@
 
-package ;
+package DataStructures;
 
+// graph using Adjency list 
 import java.util.LinkedList;
 
-public class graphs {
+class graph {
     private int vertices;
     private LinkedList<Integer> arr[] ;
    
-    public graphs(int vertices){
+    public graph(int vertices){
         this.vertices = vertices;
         
         arr = new LinkedList[vertices] ;
@@ -15,7 +16,23 @@ public class graphs {
         for(int i = 0 ; i < vertices ; i++)
             arr[i] = new LinkedList<>();
     }
-    static void addEdge(graphs g , int src , int dest){
-        
+    void addEdge(graph g , int src , int dest){
+        arr[src].add(dest);
+        arr[dest].add(src);
     }
 }
+public class graphs{
+    
+     public static void main(String args[]) 
+    {
+        int vertices = 5; 
+        graph g = new graph(vertices); 
+        g.addEdge(g, 0, 1); 
+        g.addEdge(g, 0, 4); 
+        g.addEdge(g, 1, 2); 
+        g.addEdge(g, 1, 3); 
+        g.addEdge(g, 1, 4); 
+        g.addEdge(g, 2, 3); 
+        g.addEdge(g, 3, 4); 
+    } 
+} 
