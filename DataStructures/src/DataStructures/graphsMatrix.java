bdc492e8e1f914dac9a87e5ae2dcb4b5bcfd62b1
Koshika Gupta <koshikaje@gmail.com>
2018-11-21 20:07:30 +0530

    Update graphsMatrix.java

--- DataStructures/src/DataStructures/graphsMatrix.java
+++ DataStructures/src/DataStructures/graphsMatrix.java
@@ -7,7 +7,7 @@ class Graphs{
    private int v ;
    private boolean [][] arr ;
     public Graphs(int nv){
-        v = nv ;
+        v = nv;
        arr = new boolean[v][v];
     }
     public boolean insertedge(int v1 , int v2 ){
@@ -24,12 +24,35 @@ class Graphs{
     {
         arr[v1][v2] = false;
     }
+    public void traversegraph(){
+        
+        for(int i = 0 ; i < v ; i++)
+        {
+            for(int j = 0 ; j < v ; j++)
+            {
+                if(arr[i][j] == true)
+                    System.out.println(i + " " + j + " is coonected");
+            }
+        }
+    }
 }
+
 public class graphsMatrix{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of vertices in graph");
         int nv = sc.nextInt();
-        Graphs g = new Graphs(nv);
+        Graphs g = new Graphs(nv + 1);
+        g.insertedge(0, 1); 
+        g.insertedge(0, 4); 
+        g.insertedge(1, 2); 
+        g.insertedge( 1, 3); 
+        g.insertedge( 1, 4); 
+        g.insertedge( 2, 3); 
+        g.insertedge( 3, 4); 
+        System.out.println(g.findedge(1, 2));
+        g.deleteEdge( 3 , 4);
+        System.out.println(g.findedge(3, 4));
+        g.traversegraph();
     }
 }
